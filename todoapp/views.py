089c0bc8195560755133dca25a27b6078f0e19ee
adittwidiantaraa <Widiantaraputra4@gmail.com>
2026-07-17 11:04:48 +0800

    first commit

---
+++ todoapp/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, HttpResponse, redirect
+from . form import RegisterForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+@login_required
+def home(request):
+    return render(request, "index.html")
+
+def register_view(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+
+    else:
+        form = RegisterForm()
+    return render(request, "register.html", {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('home')
+        else:
+            return render(request, "login.html") 
+
+    return render(request, "login.html")
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+def delete_task(response):
+    return HttpResponse("Deleted")
